b35c1971b9cd5d5d172decb969d9c5a4aad17866
Rashmi <rashmiuj2305@gmail.com>
2025-04-23 11:00:35 +0530

    created gradle project

---
+++ docs/script.js
@@ -0,0 +1,13 @@
+function validateLogin(){
+    var username= document.getElementById("username").value;
+    var password= document.getElementById("password").value;
+    if(username=== "admin" && password === "1234"){
+        window.location.href = "home.html";
+        return false;
+
+    }
+    else{
+        alert("invalid username or password");
+        return false;
+    }
+}
